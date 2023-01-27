ca5d7ff700557da86f33c45e1405fccfce9229db
Kate <kataroslakova10@gmail.com>
2023-01-27 17:58:51 +0300

    tast1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a: ");
+int a =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b =Convert.ToInt32(Console.ReadLine());
+
+if (a==b*b)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
